d2a9b238a70a2c32b98b5a1dcbe6c1048c7f3219
navneetthakor <tnavneer8628@gmail.com>
2023-07-28 15:57:58 +0530

    provider moved to rootlayout.js

--- src/componetn/RootLayout.js
+++ src/componetn/RootLayout.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nbar from './Nbar'
+import { Provider } from 'react-redux';
+import store from '../store/store'
 
 export default function RootLayout() {
   return (
-    <div>
+    <>
+    <Provider store={store}>
       <Nbar/>
       <main>
         <Outlet/>
       </main>
-    </div>
+      </Provider>
+    </>
   )
 }
